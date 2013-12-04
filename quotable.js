8240f0f26fc3dc19898ddfc992cde178b2f6f1cf
Eric Mann <eric@eamann.com>
2013-12-03 21:08:08 -0800

    Make Twitter work

--- quotable.js
+++ quotable.js
@@ -4,18 +4,10 @@
         tweetDiv = null,
         text = '';
     
-    function tweeter( x, y ) {       
-        if ( ! document.getElementById( 'twitter-wjs' ) ) {
-            var first = document.getElementsByTagName( 'script' )[0];
-            var script = document.createElement( 'script' );
-            script.id = 'twitter-wjs';
-            script.src = 'https://platform.twitter.com/widgets.js';
-            
-            first.parentNode.insertBefore( script, first );
-        }
-        
+    function tweeter( x, y ) {              
         if ( null !== tweetDiv ) {
-            body.removeChild( tweetDiv );
+            tweetDiv.parentNode.removeChild( tweetDiv );
+			tweetDiv = null;
         }
         
         if ( 0 === text.toString().length ) {
@@ -23,23 +15,32 @@
         }
         
         tweetDiv = document.createElement( 'div' );
-        tweetDiv.class = 'dyn-tweet-this';
+        tweetDiv.className = 'dyn-tweet-this';
         var anchor = document.createElement( 'a' );
-        anchor.class = 'twitter-share-button';
+        anchor.className = 'twitter-share-button';
         anchor.href = 'https://twitter.com/share?url=' + encodeURI( window.location ) + '&text=' + encodeURI( text );
         anchor.setAttribute( 'data-lang', 'en' );
         var label = document.createTextNode( 'Tweet This' );
         anchor.appendChild( label );
         tweetDiv.appendChild( anchor );
+		
+		var script = document.createElement( "script" );
+		script.type = "text/javascript";
+		script.src = "https://platform.twitter.com/widgets.js";
+		tweetDiv.appendChild( script );
         
         tweetDiv.style.position = 'absolute';
         tweetDiv.style.top = y + 'px';
         tweetDiv.style.left = x + 'px';
         
         body.appendChild( tweetDiv );
+		
+		// Scan for unloaded widgets.
+		//window.twttr.widgets.load();
     }
     
     function get( e ) {
+		console.log( e );
         text = document.all ? document.selection.createRange().text : document.getSelection();
         tweeter( e.pageX, e.pageY );
     }
